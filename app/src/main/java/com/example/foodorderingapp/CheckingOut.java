5e57923505e516135d6b87515fcca74ee18604e6
Rasitha Gamage <g.rasitha@gmail.com>
2019-09-02 13:33:39 +0530

    commit

--- app/src/main/java/com/example/foodorderingapp/CheckingOut.java
+++ app/src/main/java/com/example/foodorderingapp/CheckingOut.java
@@ -5,7 +5,11 @@ import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
+import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -20,11 +24,55 @@ import java.util.ArrayList;
 import java.util.Date;
 
 public class CheckingOut extends AppCompatActivity {
-
+    private NavigationView nv;
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checking_out);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Payment");
+        nv = (NavigationView) findViewById(R.id.nv);
+
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                switch (id) {
+                    case R.id.settings:
+                        Toast.makeText(CheckingOut.this, "Home", Toast.LENGTH_SHORT).show();
+                        Intent ac1 = new Intent(CheckingOut.this,Home.class);
+                        startActivity(ac1);
+                        break;
+                    case R.id.mycart:
+                        Toast.makeText(CheckingOut.this, "My Cart", Toast.LENGTH_SHORT).show();
+                        Intent ac2 = new Intent(CheckingOut.this,Cart.class);
+                        startActivity(ac2);
+                        break;
+                    case R.id.order:
+                        Toast.makeText(CheckingOut.this, "Orders", Toast.LENGTH_SHORT).show();
+                        Intent ac3 = new Intent(CheckingOut.this,Orders.class);
+                        startActivity(ac3);
+                        break;
+                    case R.id.history:
+                        Toast.makeText(CheckingOut.this, "History", Toast.LENGTH_SHORT).show();
+                        Intent ac4 = new Intent(CheckingOut.this,OrderHistory.class);
+                        startActivity(ac4);
+                        break;
+                    case R.id.logout: {
+                        Toast.makeText(CheckingOut.this, "Logout", Toast.LENGTH_SHORT).show();
+                    }
+                    default:
+                        return true;
+                }
+                return true;
+            }
+        });
+
+
+
 
         Button payByCashBtn = (Button) findViewById(R.id.payByCashBtn);
         Button DeductSalBtn = (Button) findViewById(R.id.DeductSalBtn);
